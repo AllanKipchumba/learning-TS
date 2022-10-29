18aa4892e3ca780e7b56645f6ca874e69e153f4b
_Allan <allankipchumba76@gmail.com>
2022-10-29 06:00:45 +0300

    learnt function types and callbacks

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ const add = (n1: number, n2: number) => {
   return n1 + n2;
 };
 
-//void function type > has no return vaue
+//void function type > has no return value
 const printResult = (num: number): void => console.log(`Result is ${num}`);
 
 printResult(add(5, 2));
@@ -12,3 +12,9 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+//function tyes and callbacks
+const addHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+  const result = n1 + n2;
+  cb(result);
+};
