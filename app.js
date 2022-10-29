57de380ca61f71a0c054d5bc3a459e2c5143e824
_Allan <allankipchumba76@gmail.com>
2022-10-29 05:51:56 +0300

    learnt function types

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ var add = function (n1, n2) {
 //void function type > has no return vaue
 var printResult = function (num) { return console.log("Result is ".concat(num)); };
 printResult(add(5, 2));
+//function types
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -6,3 +6,9 @@ const add = (n1: number, n2: number) => {
 const printResult = (num: number): void => console.log(`Result is ${num}`);
 
 printResult(add(5, 2));
+
+//function types
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+
+console.log(combineValues(8, 8));
